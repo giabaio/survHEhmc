57919c175f850dcccb8ed4515edd250fcf0a6c55
Gianluca Baio <g.baio@ucl.ac.uk>
2024-10-03 16:52:39 +0100

    Update and recompile the package to put in drat

---
+++ R/survHEhmc-package.R
@@ -0,0 +1,25 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+
+#' Internal objects used by stan
+#'
+#' Objects, C++ and S4 classes used by stan to fit parametric survival models.
+#'
+#'
+#' @name internal_stan
+#' @aliases internal_stan stanmodels stan_files model_Exponential model_Gamma
+#' model_GenF model_GenGamma model_Gompertz model_logLogistic model_logNormal
+#' model_PolyWeibull model_RP model_WeibullAF model_WeibullPH
+#' Rcpp_model_Exponential-class Rcpp_model_Gamma-class Rcpp_model_GenF-class
+#' Rcpp_model_GenGamma-class Rcpp_model_Gompertz-class
+#' Rcpp_model_logLogistic-class Rcpp_model_logNormal-class
+#' Rcpp_model_PolyWeibull-class Rcpp_model_RP-class Rcpp_model_WeibullAF-class
+#' Rcpp_model_WeibullPH-class
+#' @docType data
+#' @keywords internal
+NULL
